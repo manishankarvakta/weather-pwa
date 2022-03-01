1d21c99fc3f2b957510cbd0842e0505c548882fa
Manishankar Vakta <manishankarvakta@gmail.com>
2022-03-02 01:22:09 +0600

    app done

--- src/App.js
+++ src/App.js
@@ -32,7 +32,11 @@ const App = () => {
                     </h2>
                     <div className='city-temp'>
                         {Math.round(weather.main.temp)}
-                        <sup>&deg;</sup>
+                        <sup>&deg;c</sup>
+                    </div>
+                    <div className='info'>
+                        <img className='city-icon' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/> 
+                        <p>{weather.weather[0].description}</p>
                     </div>
                 </div>
             )}
